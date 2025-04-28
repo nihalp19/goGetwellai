1a2f9d4f8fe2726c0b5ee2e684134072928bb62e
Nihal Panday <nihalpanday2020@gmail.com>
2025-04-28 08:05:24 +0530

    updated vercel.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    historyApiFallback: true, // helps during local development
-  }
-
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
